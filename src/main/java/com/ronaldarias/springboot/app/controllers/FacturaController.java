412b0e6c9f455c8edd0718e94473cf5bc7926c9f
ronald.arias <ronald.arias@udla.edu.ec>
2018-03-19 10:33:47 -0500

    Validar el campo descripcion en crear factura

--- src/main/java/com/ronaldarias/springboot/app/controllers/FacturaController.java
+++ src/main/java/com/ronaldarias/springboot/app/controllers/FacturaController.java
@@ -10,10 +10,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -51,11 +53,23 @@ public class FacturaController {
     }
 
     @PostMapping("/form")
-    public String guardar(Factura factura,
+    public String guardar(@Valid Factura factura,
+          BindingResult bindingResult, Model model,
           @RequestParam(name = "item_id[]", required = false) Long[] itemId,
           @RequestParam(name = "cantidad[]", required = false) Integer[] cantidad,
           RedirectAttributes flash,
           SessionStatus status){
+
+        if(bindingResult.hasErrors()){
+            model.addAttribute("titulo", "Crear factura");
+            return "factura/form";
+        }
+
+        if(itemId == null || itemId.length == 0){
+            model.addAttribute("titulo", "Crear factura");
+            model.addAttribute("error", "Error: La factura debe tener lineas");
+            return "factura/form";
+        }
 
         for(int i = 0; i < itemId.length; i++){
             Producto producto = clienteService.findProductoById(itemId[i]);
