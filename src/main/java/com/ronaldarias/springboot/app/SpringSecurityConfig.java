8d30c7bf2a3d52200f6ab6b32cfd311c7e3b562e
ronald.arias <ronald.arias@udla.edu.ec>
2018-04-01 13:39:00 -0500

    Vista de Formulario de Login

--- src/main/java/com/ronaldarias/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/ronaldarias/springboot/app/SpringSecurityConfig.java
@@ -27,7 +27,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/factura/**").hasAnyRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().permitAll()
+                .formLogin().loginPage("/login").permitAll()
                 .and()
                 .logout().permitAll();
 
---
+++ src/main/java/com/ronaldarias/springboot/app/controllers/LoginController.java
@@ -0,0 +1,28 @@
+package com.ronaldarias.springboot.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.security.Principal;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(Model model, Principal principal, RedirectAttributes flash){
+
+        //principal permite validar
+
+        //si es distinto de null es porque ya tiene sesion iniciada
+        if(principal != null){
+            flash.addFlashAttribute("info", "Ya ha iniciado sesión previamente");
+            return "redirect:/";
+        }
+
+        model.addAttribute("titulo", "Inicio de sesión");
+        return "/login";
+    }
+
+}
