701b904fa91f85d8dae941cf6d59fdad90f3f2b6
ronald.arias <ronald.arias@udla.edu.ec>
2018-04-01 13:46:40 -0500

    Mostrar error en caso de usuario y contraseña incorrecta

--- src/main/java/com/ronaldarias/springboot/app/controllers/LoginController.java
+++ src/main/java/com/ronaldarias/springboot/app/controllers/LoginController.java
@@ -3,6 +3,7 @@ package com.ronaldarias.springboot.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
@@ -11,7 +12,8 @@ import java.security.Principal;
 public class LoginController {
 
     @GetMapping("/login")
-    public String login(Model model, Principal principal, RedirectAttributes flash){
+    public String login(@RequestParam(value = "error", required = false) String error,
+                        Model model, Principal principal, RedirectAttributes flash){
 
         //principal permite validar
 
@@ -19,6 +21,10 @@ public class LoginController {
         if(principal != null){
             flash.addFlashAttribute("info", "Ya ha iniciado sesión previamente");
             return "redirect:/";
+        }
+
+        if(error != null){
+            model.addAttribute("error", "Nombre de usuario o contraseña incorrecta");
         }
 
         model.addAttribute("titulo", "Inicio de sesión");
